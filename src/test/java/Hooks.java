944c878983ca0255457494b15f366d444972c357
Jisy Raju <jisyraju@gmail.com>
2022-02-06 12:39:37 +0000

    Creating the framework

---
+++ src/test/java/Hooks.java
@@ -0,0 +1,15 @@
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+    @Before
+    public void beforeTest()
+    {
+
+    }
+    @After
+    public void afterTest()
+    {
+
+    }
+}
